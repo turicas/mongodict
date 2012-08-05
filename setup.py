abf1fa3482b8de6ab9fcabb110729a32e6112ca1
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-08-05 00:01:32 -0300

    Replaced setuptools with distutils

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from setuptools import setup
+from distutils.core import setup
 
 
 setup(name='mongodict',
@@ -9,13 +9,11 @@ setup(name='mongodict',
       author_email='alvarojusten@gmail.com',
       url='https://github.com/turicas/mongodict/',
       description='MongoDB-backed Python dict-like interface',
-      zip_safe=True,
       py_modules=['mongodict'],
       install_requires=['pymongo'],
-      test_suite='nose.collector',
       license='GPL3',
       keywords=['key-value', 'database', 'mongodb', 'dictionary'],
-      classifiers = [
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: GNU General Public License (GPL)',
